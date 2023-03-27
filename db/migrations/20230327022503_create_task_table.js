cccd6cc768336a43b963b464fd9406d7757907f5
DOCOMO\5140801 <takanori.nagahara.ts@nttdocomo.com>
2023-03-27 12:04:53 +0900

    Modified migrations files

--- db/migrations/20230327022503_create_task_table.js
+++ db/migrations/20230327022503_create_task_table.js
@@ -5,6 +5,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable("task", function (table) {
     table.increments("id").primary();
+    table.integer("user_id").notNullable();
     table.foreign("user_id").references("id").inTable("user");
     table.string("title", 32).notNullable();
     table.string("description", 64);
